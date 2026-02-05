f3cb9fbf86967b6367307d38ec787641c7574daf
Your Name <youremail@example.com>
2026-02-05 15:24:51 -0500

    alot of changes

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
-//const menuBtn = document.querySelector('.moble-menue-icon');
-//const navLinks = document.querySelector('.nav-links');
-
-//menuBtn.addEventListener('click', () => {
-//    navLinks.classList.toggle('mobile-menu-open');
-//}); 
-
-const menuBtn = document.querySelector('.mobile-menu-icon');
+const menuBtn = document.querySelector('.moble-menue-icon');
 const navLinks = document.querySelector('.nav-links');
 
-console.log("Button found:", menuBtn); // If this says 'null', your JS can't find the button
-console.log("Links found:", navLinks); // If this says 'null', your JS can't find the menu
-
 menuBtn.addEventListener('click', () => {
-    console.log("Button was clicked!"); // If this doesn't show up in the console when you click, the click isn't registering.
-    navLinks.classList.toggle('active');
-});
+    navLinks.classList.toggle('mobile-menu-open');
+}); 
+
+//const menuBtn = document.querySelector('.mobile-menu-icon');
+//const navLinks = document.querySelector('.nav-links');
+
+//console.log("Button found:", menuBtn); // If this says 'null', your JS can't find the button
+//console.log("Links found:", navLinks); // If this says 'null', your JS can't find the menu
+
+//menuBtn.addEventListener('click', () => {
+//    console.log("Button was clicked!"); // If this doesn't show up in the console when you click, the click isn't registering.
+//    navLinks.classList.toggle('active');
+//});
+
